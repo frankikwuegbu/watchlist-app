0cf95716cfab1c262e838ca05053d86719c060e7
frankikwuegbu <99530481+frankikwuegbu@users.noreply.github.com>
2026-03-07 13:57:41 +0100

    chore: scalar configuration

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,16 +1,23 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "Watchlist App";
+        options.HideClientButton = true;
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/", () => "hello world");
 
 app.Run();
