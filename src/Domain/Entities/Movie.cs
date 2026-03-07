73f19cbf5d0ee76d9245dea8f8004eb33b678e19
frankikwuegbu <99530481+frankikwuegbu@users.noreply.github.com>
2026-03-07 15:28:14 +0100

    feat: entities created

---
+++ src/Domain/Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities;
+
+public class Movie
+{
+    public int Id { get; private set; }
+    public int WatchlistId { get; private set; }
+    public int TmdbId { get; private set; }
+    public string Title { get; private set; }
+    public string ReleaseDate { get; private set; }
+    public string Overview { get; private set; }
+    public double Rating { get; private set; }
+
+    private Movie() { }
+}
---
+++ src/Domain/Entities/Watchlist.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities;
+
+public class Watchlist
+{
+    public int Id { get; set; }
+
+    private readonly List<Movie> _movies = [];
+    public IReadOnlyCollection<Movie> Movies => _movies.AsReadOnly();
+
+    private Watchlist() { }
+
+    public void AddMovie(Movie movie)
+    {
+        _movies.Add(movie); //do more here!
+    }
+}
