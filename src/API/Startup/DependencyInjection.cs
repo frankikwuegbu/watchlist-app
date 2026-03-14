26b4ec8c2296f3caceaaa5451373f408e0c736fa
frankikwuegbu <99530481+frankikwuegbu@users.noreply.github.com>
2026-03-14 15:42:19 +0100

    feat: Register Third Party Api

--- src/API/Startup/DependencyInjection.cs
+++ src/API/Startup/DependencyInjection.cs
@@ -1,9 +1,18 @@
-﻿namespace Watchlist_App.Startup;
+﻿using Application.Common.Interface;
+using Infrastructure.Services;
+
+namespace Watchlist_App.Startup;
 
 public static class DependencyInjection
 {
     public static void AddDependencies(this WebApplicationBuilder builder)
     {
         builder.Services.AddOpenApiServices();
+
+        //tmdb service
+        builder.Services.AddHttpClient<ITmdbServices, TmdbServices>(client =>
+        {
+            client.BaseAddress = new Uri("https://api.themoviedb.org/3");
+        });
     }
 }
